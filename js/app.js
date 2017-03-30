ced12561c3deecf910d087794be9f916ec1d95b5
Jay Schwane <jrschwane@uwalumni.com>
2017-03-29 23:27:55 -0500

    rework most of the portfolio works

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+/* jshint esversion:6 */
+/* globals angular */
+
 (function () {
     
     'use strict';
@@ -10,44 +13,69 @@
         
             $scope.works = [
                 {
-                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio-csv_parse.png",
+                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_dev-dash.jpg",
+                    title: "dev-dash",
+                    description: "A Chrome browser new-tab override that presents a brief dashboard view of a selected GitHub repository whenever you open a new tab. Tech used: modular patterns, jQuery, ES6, promises, ajax, SVG, local storage, Gulp.",
+                    githubUrl: "https://github.com/general-meerkats/dev-dash",
+                    demoUrl: "https://chrome.google.com/webstore/detail/dev-dash/dgkmjidcaelocfmnhlmeimmbaljgjfhm"
+                },
+                {
+                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_momentum.jpg",
+                    title: "Meerkat Momentum",
+                    description: "Meerkat Momentum is a reverse-engineered version of the popular productivity tool Momentum Dash. Tech used: modular patterns, jQuery, ES6, promises, lots of ajax, local storage.",
+                    githubUrl: "https://github.com/general-meerkats/meerkat_momentum",
+                    demoUrl: "https://general-meerkats.github.io/meerkat_momentum/"
+                },
+                {
+                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_pfapp.jpg",
+                    title: "PFapp",
+                    description: "PFapp (Personal Finance App) is three personal finance utilities: a financial independence calculator, a comparative cost of commuting calculator and a portfolio tracker. Tech used: Angular 1.x, promises, Google Maps Distance Matrix API, SVG, local storage.",
+                    githubUrl: "https://github.com/belcurv/pfapp",
+                    demoUrl: "http://belcurv.com/pfapp/"
+                },
+                {
+                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_fibonacci.png",
+                    title: "Fibonacci Tribute",
+                    description: "A dynamic Angular app that calculates the Fibonacci sequence and builds the famous spiral as the user manipulates the range slider. Tech used: Angular 1.x, SVG.",
+                    githubUrl: "https://github.com/belcurv/fibonacci",
+                    demoUrl: "https://belcurv.github.io/fibonacci/"
+                },
+                {
+                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_album_app_md.png",
+                    title: "Album Aggregator",
+                    description: "Angular 1.x single page application served by Node / Express. The SPA consumes an API and includes basic CRUD functions for maintaining a music collection. Tech used: Angular 1.x, Node/Express, Mongo/Mongoose, Material Design.",
+                    githubUrl: "https://github.com/belcurv/album_app_md",
+                    demoUrl: "http://jrs-album-app-md.herokuapp.com/"
+                },
+                {
+                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_calculator.png",
+                    title: "JavaScript Calculator",
+                    description: "A basic calculator that uses the Shunting Yard algorithm to parse infix expressions into postfix / RPN notation to evaluate them respectful of operator precedence. Tech used: modular patterns, jQuery, ES6, regex",
+                    githubUrl: "https://github.com/belcurv/fcc-calculator",
+                    demoUrl: "https://belcurv.github.io/fcc-calculator/"
+                },
+                {
+                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_simon.png",
+                    title: "Simon Game",
+                    description: "A clone of the classic Simon memory game. Tech used: modular patterns, jQuery, ES6, regex",
+                    githubUrl: "https://github.com/belcurv/fcc-simon",
+                    demoUrl: "https://belcurv.github.io/fcc-simon/"
+                },
+                {
+                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_csv-parse.png",
                     title: "CSV Parser",
-                    description: "This Angular app reads in a remote CSV file via XHR, parses the file using Papaparse, formats the results, outputs the contents as a table and raw JSON. A HTML5 input/range slider is used to filter the results by specific dates.",
+                    description: "Angular app that reads a remote CSV file via XHR, parses it using Papaparse, formats and outputs the result as a table and raw JSON. A HTML5 input/range slider is used to filter the results by specific dates. Tech used: Angular 1.x, promises, Material Design Lite.",
                     githubUrl: "https://github.com/belcurv/csv_parse",
                     demoUrl: "http://belcurv.com/csv_parse/"
                 },
                 {
-                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_album_app_auth.png",
-                    title: "MEAN App with Authentication",
-                    description: "An Angular application served by Node and Express that uses JWT for user authentication.  It's a basic CRUD application for maintaining a music collection.",
-                    githubUrl: "https://github.com/belcurv"
-                },
-                {
                     imageUrl: "http://belcurv.com/portfolio/assets/portfolio_fishtank.png",
                     title: "Interactive Fish Tank",
-                    description: "This is a simple application that uses the processing.js library to create a dynamic interactive fish tank. The seaweed, bubbles and initial fish size/color/direction are random. Refresh the page to start over with different everything. You can add new randomly-generated fish by clicking anywhere inside the fish tank. bigger things move faster (bigger bubbles rise faster and bigger fish swim faster). When the bubbles reach the top, the start over at the bottom. When any fish reaches the left or right side of the tank, it reverses direction.",
+                    description: "An interactive fish tank. Users can add a random fish by clicking on the screen. Tech used: processing.js.",
                     githubUrl: "https://github.com/belcurv/fish-tank",
                     demoUrl: "http://belcurv.com/portfolio/assets/fish_tank/fishtank.html"
-                },
-                {
-                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_album_app_md.png",
-                    title: "MEAN App with Angular Material",
-                    description: "This is a no-authentication version of the music collection application using Angular Material instead of Bootstrap.",
-                    githubUrl: "https://github.com/belcurv/album_app_md"
-                },
-                {
-                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_dice_roll.png",
-                    title: "Interactive Dice Roll Sim",
-                    description: "This is a simple application using the processing.js library to simulate rolling two 6-sided dice. It counts and sums the results, tabulates everything, and graphs the cumulative sums. No surprise, the distribution takes the shape of a bell curve if you feed it enough dice rolls.",
-                    githubUrl: "https://github.com/belcurv/dice-roll",
-                    demoUrl: "http://belcurv.com/portfolio/assets/dice_roll/dice-roll.html"
-                },
-                {
-                    imageUrl: "http://belcurv.com/portfolio/assets/portfolio_basic_ejs.png",
-                    title: "Boilerplate static site with EJS templates",
-                    description: "This is a simple static site built witn NodeJS and Express on the server, serving EJS templates with Material Design Lite.",
-                    githubUrl: "https://github.com/belcurv/basic_ejs"
                 }
+                
             ];
             
             $scope.contacts = [
